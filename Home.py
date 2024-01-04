7ca0d57eea44cc2a56da5384344ecd9df42e2a2b
Max Modlin <max.modlin@gmail.com>
2024-01-04 08:04:49 +0000

    Enabled auth form.

--- Home.py
+++ Home.py
@@ -5,31 +5,31 @@ import os
 from PIL import Image
 import hmac
 import json
-# def check_password():
-#     """Returns `True` if the user had the correct password."""
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-#     def password_entered():
-#         """Checks whether a password entered by the user is correct."""
-#         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-#             st.session_state["password_correct"] = True
-#             del st.session_state["password"]  # Don't store the password.
-#         else:
-#             st.session_state["password_correct"] = False
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
 
-#     # Return True if the passward is validated.
-#     if st.session_state.get("password_correct", False):
-#         return True
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
 
-#     # Show input for password.
-#     st.text_input(
-#         "Password", type="password", on_change=password_entered, key="password"
-#     )
-#     if "password_correct" in st.session_state:
-#         st.error("😕 Password incorrect")
-#     return False
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
 
-# if not check_password():
-#     st.stop()  # Do not continue if check_password is not True.
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 # Page title
 st.title('🏠 Property Vision')
